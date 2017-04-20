80a9413bb4ea2552f5a1d31a44bf60de286fe260
aumaum666 <aumaum666@hotmail.com>
2017-04-20 11:47:55 +0700

    Test Map

--- app/src/main/java/comsci/kalin/connectmap/CallMap.java
+++ app/src/main/java/comsci/kalin/connectmap/CallMap.java
@@ -1,7 +1,11 @@
 package comsci.kalin.connectmap;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CallMap extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class CallMap extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_map);
     }  //on Create Method
+
+    public void runCallMap(View view){
+
+        //Initial View
+        EditText latitude = (EditText) findViewById(R.id.mapLat);
+        EditText longtitude = (EditText) findViewById(R.id.mapLong);
+        EditText lable = (EditText) findViewById(R.id.mapLable);
+
+        String lat = latitude.getText().toString().trim();
+        String lng = longtitude.getText().toString().trim();
+        String mlable = lable.getText().toString().trim();
+
+        Uri location = Uri.parse("http://maps.google.com/maps?q=loc:13.759617,100.542439(ศูนย์รางน้ำ)");
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+        //mapIntent.setPackage("com.google.android.apps.maps");  // ใช้ไม่ได้กับ GenyMotion
+        startActivity(mapIntent);
+
+    }  //runCallMap
 
 }  // Main Class
